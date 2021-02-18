aff0bf37195bc2ece1b2fc6f21caec488e63c759
Vallghall <romangusev2001@mail.ru>
2021-02-18 17:37:46 +0300

    Исправления функции задания 2

--- labtask/labtask_func2.cpp
+++ labtask/labtask_func2.cpp
@@ -1,8 +1,5 @@
 #include <math.h>
 double f(double x)
 {
-	if (x <= 3)
-		return (pow(x,2) - 3*x + 9);
-	else
-		return (1/(pow(x,3) + 6));
+		return (x <= 3 ? pow(x,2) - 3 * x + 9 : 1 / (pow(x, 3) + 6));
 }
